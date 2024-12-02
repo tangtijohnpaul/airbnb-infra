2662c48672ced819bf6c5299bed08b519c6e18df
johnpaul tangti <tangti_johnpaul@yahoo.com>
2024-12-02 14:47:54 -0600

    modifications

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "airbnb-web" {
     Name = "airbnb-web-server"
   }
 }
+
+resource "aws_instance" "airbnb-we" {
+  ami           =var.ami_id
+  instance_type = var.instance_type
+  
+  tags = {
+    Name = "airbnb-web-server"
+  }
+}
