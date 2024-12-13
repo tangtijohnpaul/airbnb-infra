37f1dd28c8b00794169f068bfd33cae17fefc55f
johnpaul tangti <tangti_johnpaul@yahoo.com>
2024-12-12 18:57:42 -0600

    more instances

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,16 @@ resource "aws_instance" "airbnb-web" {
   }
 }
 
-resource "aws_instance" "airbnb-we" {
+resource "aws_instance" "airbnb-prod" {
+  ami           =var.ami_id
+  instance_type = var.instance_type
+  
+  tags = {
+    Name = "airbnb-web-server"
+  }
+}
+
+resource "aws_instance" "airbnb-stage" {
   ami           =var.ami_id
   instance_type = var.instance_type
   
